d59035a87ffa2749bf647670a9630047d4f52ed2
davideitel <david@masterstreet.com>
2014-09-16 16:00:09 -0400

    Allow qa environment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.getEnv = getEnv
 function getEnv () {
   var env = process.env.BROCCOLI_ENV || 'development'
-  if (env !== 'production' && env !== 'development') {
+  if (env !== 'production' && env !== 'development' && env !== 'qa') {
     throw new Error('Environment set to "' + env + '", but only BROCCOLI_ENV=production and BROCCOLI_ENV=development are supported at the moment')
   }
   return env
